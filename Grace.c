e5f33d56ee03db60ef3d141ae75de248bd8a413c
Hugo DELABY <hdelaby@e2r7p3.42.fr>
2018-01-23 13:43:24 +0100

    Splitted in dirs

--- Grace.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <fcntl.h>
-#define A "Grace_kid.c"
-#define B 1537
-/*
-   TATA
-*/
-#define FT()int main(){int f=open(A, B, 0644);char *a="#include <stdio.h>%c#include <fcntl.h>%c#define A %cGrace_kid.c%c%c#define B 1537%c/*%c   TATA%c*/%c#define FT()int main(){int f=open(A, B, 0644);char *a=%c%s%c;dprintf(f,a,10,10,34,34,10,10,10,10,10,34,a,34,10,10);}%cFT()%c";dprintf(f,a,10,10,34,34,10,10,10,10,10,34,a,34,10,10);}
-FT()
