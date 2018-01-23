f225ee76e7fee0dffa32d20b8abf4adcfe2ab9a4
Hugo DELABY <hdelaby@e2r7p3.42.fr>
2018-01-23 10:16:50 +0100

    WIP Sully

---
+++ Sully.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <fnctl.h>
+#include <stdlib.h>
+int	main(void){
+int i=5, j=i-1;
+char str[12];
+char a[100];
+if (i>=0)
+{
+	sprintf(a, "Sully_%d", i);
+	strcpy(s, "Sully_");
+	strcat(s, str);
+	int fd = open(s, 1537, 0644);
+	dprintf(fd, "int i = %d; j = i - 1", );
+	system("clang -Wall -Wextra -Werror Sully_);
+	execve("./Gr"
+}
+}
